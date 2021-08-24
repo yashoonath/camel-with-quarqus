4100a865223d3840f9f6fa4f1d304c4737237f3e
yashoonath <yashoonath@gmail.com>
2021-08-24 21:28:34 +1000

    added basic auth

--- src/test/java/org/myapp/GreetingResourceTest.java
+++ src/test/java/org/myapp/GreetingResourceTest.java
@@ -12,10 +12,31 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
+                .auth().basic("test", "test")
           .when().get("/hello/Tom")
           .then()
              .statusCode(200)
              .body(is("Hi Tom"));
+    }
+
+    @Test
+    public void testInValidCredentialsHelloEndpoint() {
+        //401 authentication failure bad password
+        given()
+                .auth().basic("admin", "user")
+                .when().get("/hello/Tom")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    public void testInValidUserHelloEndpoint() {
+        //403 authenticated but not authorised response
+        given()
+                .auth().basic("user", "user")
+                .when().get("/hello/Tom")
+                .then()
+                .statusCode(403);
     }
 
 }
